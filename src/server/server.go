145ea2dff94547fbd3dbfa12da2b346925bf4d7d
VadirajaRao <vadiraja98rao@gmail.com>
2019-10-12 15:17:08 +0530

    updated form part of the code, with comments

--- src/server/server.go
+++ src/server/server.go
@@ -13,6 +13,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// This structure is used to store the login information submitted by the user
+// in the login.html form.
 type Login struct {
 	Username string
 	Password string
@@ -26,22 +28,44 @@ func indexPageHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+// Function to handle the information page
 func informationPageHandler(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./templates/info.html"))
 
 	t.Execute(w, nil)
 }
 
+// Function to handle the login page
+// This function verifies the user who wants to login to his/her account.
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("./templates/info.html"))
+	t := template.Must(template.ParseFiles("./templates/login.html"))
 
+	// If the HTTP request is using GET method, then the login form will be loaded
 	if r.Method != http.MethodPost {
-		t := template.Must(template.ParseFiles("./templates/login.html"))
 		t.Execute(w, nil)
 		return
 	}
+	// The following section will be executed once the form has been submitted.
+	// Since the form uses POST method to submit the form content, the above
+	// section will not be executed.
 
-	t.Execute(w, nil)
+	// This statement reads the value once the form is submitted.
+	name := r.FormValue("username")
+
+	// The following structure is used to send data back to the html page to show
+	// a particular message. These values are made use in the if-else block in
+	// login.html
+	type Variables struct {
+		Success bool
+		Name string
+	}
+
+	x := Variables {
+		Success: true,
+			Name: name,
+		}
+
+	t.Execute(w, x)
 }
 
 func main() {
